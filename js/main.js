68782a6af6c7042556bd8869e4b4da98be6cc9e3
Arpit <arpit_jain@live.com>
2018-04-23 13:54:36 +0530

    Contact form on multiple button. Font smoothing.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,33 @@ $('body').on('click', '#contact-form-anchor', function(e) {
     e.preventDefault();
     if ($(this).hasClass('active')) {
         $(this).removeClass('active');
-        $('.fixed-contact-form').hide();
+        $(this).animate({
+            top: '50%'
+        }, 200);
+        $('.fixed-contact-form').fadeOut(400);
     } else {
         $(this).addClass('active');
-        $('.fixed-contact-form').show();
+        $(this).animate({
+            top: $('.fixed-contact-form').offset().top
+        }, 200);
+        $('.fixed-contact-form').fadeIn(400);
+    }
+});
+
+$('body').on('click', '.show-enquiry-form', function(e) {
+    e.preventDefault();
+    var _this = $('#contact-form-anchor');
+    if ($(_this).hasClass('active')) {
+        $(_this).removeClass('active');
+        $(_this).animate({
+            top: '50%'
+        }, 200);
+        $('.fixed-contact-form').fadeOut(400);
+    } else {
+        $(_this).addClass('active');
+        $(_this).animate({
+            top: $('.fixed-contact-form').offset().top
+        }, 200);
+        $('.fixed-contact-form').fadeIn(400);
     }
 });
